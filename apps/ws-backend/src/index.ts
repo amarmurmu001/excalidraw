287db363ba6549b4b6651c6fc842af40e1e1a7a5
amarmurmu001 <iron57441@gmail.com>
2025-07-14 19:45:50 +0530

    feat:ws-connection and messages working

--- apps/ws-backend/src/index.ts
+++ apps/ws-backend/src/index.ts
@@ -1,25 +1,42 @@
+import { Room } from './../../../packages/db/generated/prisma/index.d';
 import  jwt, { decode, JwtPayload }  from 'jsonwebtoken';
-import { WebSocketServer } from 'ws';
+import { WebSocketServer,WebSocket } from 'ws';
 import { JWT_SECRET } from '@repo/backend-common/config';
+
+interface User{
+  ws: WebSocket;
+  userId: string;
+  room: string[];
+}
+
+const users: User[] = [];
 
 const wss = new WebSocketServer({ port: 8080 });
 
+
 function checkUser(token: string): string | null{
-   const decoded = jwt.verify(token,JWT_SECRET);
+  try{
 
-   if(typeof decoded === 'string'){
-        return null;
+    const decoded = jwt.verify(token,JWT_SECRET);
+    
+    if(typeof decoded === 'string'){
+      return null;
     }
-
+    
     if(!decoded || !(decoded as JwtPayload).userId){
-        return null;
+      return null;
     }
-
+    
     return decoded.userId;
   
+  }catch(e){
+    console.error('Error verifying token:', e);
+    return null;
+  }
+  return null;
 }
-
-wss.on('connection', function connection(ws, request) {
+  
+  wss.on('connection', function connection(ws, request) {
   const url = request.url;
   if(!url){
     return;
@@ -31,13 +48,50 @@ wss.on('connection', function connection(ws, request) {
   const userId = checkUser(token);
 
   if (!userId) {
-    ws.close(1008, 'Authentication failed');
-    return;
+    ws.close();
+    return null;
   }
+
+  users.push({
+    userId: userId,
+    room: [],
+    ws
+  });
+
+
 
 
   ws.on('message', function message(data){
-    ws.send("Ping Pong");
+    const parsedData = JSON.parse(data as unknown as string);
+    
+    if(parsedData.type === 'join_room'){
+      const user = users.find(x => x.ws === ws);
+      user?.room.push(parsedData.roomId);
+    }
+
+    if(parsedData.type === 'leave_room'){
+      const user = users.find(x => x.ws === ws);
+      if(!user){
+        return;
+      }
+      user.room = user?.room.filter(x => x !== parsedData.roomId);
+    }
+
+    if(parsedData.type === 'chat'){
+      const roomId = parsedData.roomId;
+      const messsage = parsedData.message;
+
+      users.forEach(user => {
+        if(user.room.includes(roomId)){
+          user.ws.send(JSON.stringify({
+            type: 'chat',
+            roomId,
+            message: messsage,
+          }));
+        }
+      })
+
+    }
   });
 
  
